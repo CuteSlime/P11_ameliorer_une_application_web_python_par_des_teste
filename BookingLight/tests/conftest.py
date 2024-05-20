bb9d72ce68a1b55c8549b38ec055786baf6acdb5
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-05-20 23:30:54 +0200

    Add Pytest with base data

---
+++ BookingLight/tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+
+from BookingLight import app
+
+
+@pytest.fixture(scope="session")
+def flask_app():
+    app
+
+    client = app.test_client()
+
+    ctx = app.test_request_context()
+    ctx.push()
+
+    yield client
+
+    ctx.pop()
+
+
+@pytest.fixture(scope="session")
+def app_with_db(flask_app):
+
+    club = {
+        'name': 'Test Club',
+        'email': 'test@club.com',
+        'points': '15'
+    }
+    competition = {
+        'name': 'Test Competition',
+        'date': '2024-06-15',
+        'numberOfPlaces': '10'
+    }
+
+    yield flask_app, [competition], [club]
