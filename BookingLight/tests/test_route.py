3d53093563fa52c1487f202ab92f09b1ed5a4a85
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-05-21 21:37:35 +0200

    test if not enough point to book

--- BookingLight/tests/test_route.py
+++ BookingLight/tests/test_route.py
@@ -25,3 +25,37 @@ def test_purchasePlaces(mock_loadClubs, mock_loadCompetitions, mock_writeClubs, 
     assert updated_competition['numberOfPlaces'] == '5'
     mock_writeClubs.assert_called_once_with(mock_clubs)
     mock_writeCompetitions.assert_called_once_with(mock_competitions)
+
+
+@patch('BookingLight.views.writeCompetitions')
+@patch('BookingLight.views.writeClubs')
+@patch('BookingLight.views.loadCompetitions')
+@patch('BookingLight.views.loadClubs')
+def test_purchasePlaces_not_enough_point(mock_loadClubs,
+                                         mock_loadCompetitions,
+                                         mock_writeClubs,
+                                         mock_writeCompetitions,
+                                         app_with_db
+                                         ):
+
+    flask_app, mock_competitions, mock_clubs = app_with_db
+
+    mock_competitions[0]['numberOfPlaces'] = '25'
+    mock_clubs[0]['points'] = '15'
+
+    mock_loadClubs.return_value = mock_clubs
+    mock_loadCompetitions.return_value = mock_competitions
+
+    response = flask_app.post('/purchasePlaces', data={
+        'club': 'Test Club',
+        'competition': 'Test Competition',
+        'places': '20'
+    }, follow_redirects=True)
+
+    assert response.status_code == 200
+    assert b'<title>Booking for Test Competition || GUDLFT</title>' in response.data
+
+    updated_club = mock_clubs[0]
+    updated_competition = mock_competitions[0]
+    assert updated_club['points'] == '15'
+    assert updated_competition['numberOfPlaces'] == '25'
